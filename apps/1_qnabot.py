4b84d3b606995096ff062e207bdc3eb0d18bb79d
Prajaktha <prajaktha3@gmail.com>
2026-09-21 18:04:40 +0530

    Add files via upload

--- apps/1_qnabot.py
+++ apps/1_qnabot.py
@@ -1,1 +1,11 @@
+#loading the environmental file
+from dotenv import load_dotenv
+load_dotenv()
 
+#Create a ChatGroq object and invoke it
+from langchain_groq import ChatGroq
+llm=ChatGroq(model="openai/gpt-oss-120b")
+
+import streamlit as st
+st.title("IntelliChat")
+st.markdown("It assists me in learning GenAI")
